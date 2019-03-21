6334f104a94a622460de4f7419ad248728e91f5a
Demon <demonli@tvunetworks.com>
2019-03-21 14:20:43 +0800

    Create Project

---
+++ XDXTextContentHandler/XDXTextContentHandler/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XDXTextContentHandler
+//
+//  Created by 小东邪 on 2019/3/21.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ XDXTextContentHandler/XDXTextContentHandler/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XDXTextContentHandler
+//
+//  Created by 小东邪 on 2019/3/21.
+//  Copyright © 2019 小东邪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
